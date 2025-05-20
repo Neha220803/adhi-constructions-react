84b006bd9b29cacc67235ce4bf5cf47a365f8a48
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-05-20 12:56:47 +0530

    Description changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
